e5da619a6ff77049a61418589381b4d36312b466
soikat-san <soikat.codes@gmail.com>
2025-05-04 14:33:39 +0530

    container header

--- microui/container/src/app.js
+++ microui/container/src/app.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    <div>
-      <h1>hi there, container triggered, hell yea !!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
