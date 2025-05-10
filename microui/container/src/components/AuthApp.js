afac68bcfef55e083b0b14b29a95b3697a708317
soikat-san <soikat.codes@gmail.com>
2025-05-10 11:57:10 +0530

    auth script

--- microui/container/src/components/AuthApp.js
+++ microui/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from "auth/AuthApp";
 import { useHistory } from "react-router-dom";
 import React, { useRef, useEffect } from "react";
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -15,6 +15,9 @@ export default () => {
         if (pathname !== nextPathname) {
           history.push(nextPathname);
         }
+      },
+      onSignIn: () => {
+        onSignIn();
       },
     });
 
